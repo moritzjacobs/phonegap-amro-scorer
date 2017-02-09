08f15d4677b6336b668226faf17f043d52076b21
Moritz Jacobs <mail@moritzjacobs.de>
2017-02-09 14:44:51 +0100

    fix bash scripts

--- emulator.sh
+++
@@ -1,4 +0,0 @@
-echo "android avd" starts emulator
-cd $ANDROID_SDK/tools
-# adb uninstall android-debug.apk
-adb install android-debug.apk
--- open_in_emulator.sh
+++ open_in_emulator.sh
@@ -1,4 +1,5 @@
+source ~/.bash_profile
 echo "android avd" starts emulator
 cd $ANDROID_SDK/tools
-adb uninstall de.moritzjacobs.pg.amroscorer
+adb uninstall de.moritzjacobs.pg.appname
 adb install android-debug.apk
