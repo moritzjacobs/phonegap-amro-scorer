fbf70b0d1d3129b12005a07729b5a8045c4f097a
Moritz Jacobs <mail@moritzjacobs.de>
2017-02-09 13:12:12 +0100

    improve shell scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+source ~/.bash_profile
 cd _dev; gulp dist; cd ..;
 phonegap build android --debug --device
-cp ./platforms/android/build/outputs/apk/android-debug.apk /usr/local/Cellar/android-sdk/24.4.1_1/tools/
-mv ./platforms/android/build/outputs/apk/android-debug.apk /Users/mo/RealDropbox/Dropbox/
+cp ./platforms/android/build/outputs/apk/android-debug.apk $ANDROID_SDK/tools/
+mv ./platforms/android/build/outputs/apk/android-debug.apk $DROPBOX_HOME/android-debug/
--- open_in_emulator.sh
+++ open_in_emulator.sh
@@ -1,4 +1,4 @@
 echo "android avd" starts emulator
 cd $ANDROID_SDK/tools
-# adb uninstall android-debug.apk
+adb uninstall de.moritzjacobs.pg.amroscorer
 adb install android-debug.apk
